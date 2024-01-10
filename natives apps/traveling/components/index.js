1459c46bf30c297605a1da56b29a9b2d886876c7
William Hortone <willhortone@gmail.com>
2024-01-11 05:15:30 +0800

    completed the button component

--- natives apps/traveling/components/index.js
+++ natives apps/traveling/components/index.js
@@ -1,4 +1,6 @@
-// import Slides from "./slides/Slides";
+import ReusableBtn from "./button/ReusableBtn";
+import HeightSpacer from "./reusable/HeightSpacer";
 import ReusableText from "./reusable/ReusableText";
+import WidthSpacer from "./reusable/WidthSpacer";
 
-export { ReusableText };
+export { ReusableText, ReusableBtn, HeightSpacer, WidthSpacer };
--- natives apps/traveling/components/slides/Slides.jsx
+++ natives apps/traveling/components/slides/Slides.jsx
@@ -1,8 +1,8 @@
 import { Text, View, Image } from "react-native";
 import React from "react";
 import styles from "./slides.style";
-import { ReusableText } from "../../components";
-import { COLORS, SIZES } from "../../constants/index";
+import { HeightSpacer, ReusableBtn, ReusableText } from "../../components";
+import { COLORS, SIZES } from "../../constants";
 
 const Slides = ({ item }) => {
   return (
@@ -16,7 +16,16 @@ const Slides = ({ item }) => {
           family={"medium"}
           color={COLORS.white}
         />
-        {/* <Text>ohljkdhfjkhdjlkfdkfhdjfhdjkfhdjkfh</Text> */}
+        <HeightSpacer height={40} />
+        <ReusableBtn
+          onPress={() => {}}
+          btnText={"Get started"}
+          width={(SIZES.width - 50) / 2.2}
+          bgColor={COLORS.red}
+          borderColor={COLORS.red}
+          borderWidth={0}
+          textColor={COLORS.white}
+        />
       </View>
     </View>
   );
