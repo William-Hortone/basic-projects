1f40a30e1342ced63640c03bd44d066a71837643
William Hortone <willhortone@gmail.com>
2024-02-13 13:18:19 +0800

    added the  auth controllers

--- natives apps/backend/controllers/authControllers.js
+++ natives apps/backend/controllers/authControllers.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
-
 const CryptoJs = require("crypto");
+// const { request } = require("http");
 const jwt = require("jsonwebtoken");
 
 module.exports = {
@@ -8,15 +8,54 @@ module.exports = {
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: CryptoJs.AES.encrypt(req.body.password, process.env.SECRET_KEY),
+      password: CryptoJs.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+      ).toString(),
     });
 
     try {
       await newUser.save();
-
       res
         .status(201)
         .json({ status: true, message: "User created successfully" });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  loginUser: async (req, res, next) => {
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        return res
+          .status(404)
+          .json({ status: false, message: "user not found" });
+      }
+
+      const decryptedPassword = CryptoJs.AES.decrypt(
+        user.password,
+        process.env.SECRET_KEY
+      );
+      const decryptedString = decryptedPassword.toString(CryptoJs.enc.utf8);
+
+      if (decryptedString !== request.body.password) {
+        return res
+          .status(404)
+          .json({ status: false, message: "Wrong password" });
+      }
+
+      const userToken = jwt.sign(
+        {
+          id: user._id,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "21d" }
+      );
+
+      const user_id = user._id;
+
+      res.status(200).json({ status: true, id: user_id, token: userToken });
     } catch (error) {
       return next(error);
     }
--- natives apps/backend/server.js
+++ natives apps/backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const port = 3000;
 const mongoose = require("mongoose");
 const errorHandler = require("./middleware/errorHandling");
+const authRouter = require("./routes/auth");
+const port = 3000;
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(errorHandler);
+
+app.use("/api/", authRouter);
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(process.env.PORT || port, () =>
   console.log(`App listening on port ${process.env.PORT}!`)
